c1f93f06375882b9f9320c9b85474f3853454f85
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-01-08 21:09:00 +0545

    Till day 12

--- JAVA_SCRIPT/jscode.js
+++ JAVA_SCRIPT/jscode.js
@@ -1,285 +1,288 @@
-// // Day 6 // arrays in JS
+// Day 6 // arrays in JS
 
-// let arr = [1, 2, 3, 4, 5];
-// console.log(arr);
-// console.log(typeof arr); // object
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(typeof arr); // object
 
-// // accessing elements
-// console.log(arr[0]); // 1
-// console.log(arr[2]); // 3
-// console.log(arr[4]); // 5
-// console.log(arr[5]); // undefined
-// // modifying elements
-// arr[2] = 10;
-// console.log(arr); // [1,2,10,4,5]
-// // length of array
-// console.log(arr.length); // 5
-// // adding elements
-// arr.push(6);
-// console.log(arr); // [1,2,10,4,5,6]
-// // removing elements
-// arr.pop();
-// console.log(arr); // [1,2,10,4,5]
+// accessing elements
+console.log(arr[0]); // 1
+console.log(arr[2]); // 3
+console.log(arr[4]); // 5
+console.log(arr[5]); // undefined
+// modifying elements
+arr[2] = 10;
+console.log(arr); // [1,2,10,4,5]
+// length of array
+console.log(arr.length); // 5
+// adding elements
+arr.push(6);
+console.log(arr); // [1,2,10,4,5,6]
+// removing elements
+arr.pop();
+console.log(arr); // [1,2,10,4,5]
 
-// for(let idx=0; idx<arr.length; idx++){
-//     console.log(`Element at index ${idx} is ${arr[idx]}`);
-// }
+for(let idx=0; idx<arr.length; idx++){
+    console.log(`Element at index ${idx} is ${arr[idx]}`);
+}
 
-// // practice problem:
+// practice problem:
 
-// let marks=[85,97,44,37,76,60]
-// let average=0;
-// for (let i=0 ; i <marks.length;i++){
-//      average+=marks[i]/marks.length;
-// }
-//     console.log(average);
+let marks=[85,97,44,37,76,60]
+let average=0;
+for (let i=0 ; i <marks.length;i++){
+     average+=marks[i]/marks.length;
+}
+    console.log(average);
 
-// let price=[250,645,300,900,50];
+let price=[250,645,300,900,50];
 
-// for(let i=0;i<price.length;i++){
-//      console.log(`original price at index ${i}:${price[i]}`);
-//      price[i]=price[i]*0.9; // adding 10% discount price
-//      console.log(`discounted price at index ${i}:${price[i]}`);
-// }
+for(let i=0;i<price.length;i++){
+     console.log(`original price at index ${i}:${price[i]}`);
+     price[i]=price[i]*0.9; // adding 10% discount price
+     console.log(`discounted price at index ${i}:${price[i]}`);
+}
 
-// let marvelhero=['ironman','spiderman','thor','hulk','captain america'];
-// let dchero=['batman','superman','flash','aquaman','wonder woman'];
+let marvelhero=['ironman','spiderman','thor','hulk','captain america'];
+let dchero=['batman','superman','flash','aquaman','wonder woman'];
 
-// let allhero=marvelhero.concat(dchero);
-// console.log(allhero);
+let allhero=marvelhero.concat(dchero);
+console.log(allhero);
 
-// let val=marvelhero.shift();
-// console.log(val);
-// marvelhero.unshift('black panther');
-// console.log(marvelhero);
-// let rev=marvelhero.reverse();
-// console.log(rev);
+let val=marvelhero.shift();
+console.log(val);
+marvelhero.unshift('black panther');
+console.log(marvelhero);
+let rev=marvelhero.reverse();
+console.log(rev);
 
-// console.log(marvelhero.slice(2,4)); 
-// marvelhero.splice(1,'spiderman','black widow');
-// console.log(marvelhero); 
+console.log(marvelhero.slice(2,4)); 
+marvelhero.splice(1,'spiderman','black widow');
+console.log(marvelhero); 
 
-// // practice problem:
-// let companies=['google','meta','apple','microsoft','amazon'];
-// companies.pop(1);
-// console.log(companies);
-// companies.shift();
-// companies.splice(1,2,'netflix','spotify');
-// console.log(companies);
-// companies.push("tesla");
-// console.log(companies);
-// console.log(companies.indexOf('amazon'));
+// practice problem:
+let companies=['google','meta','apple','microsoft','amazon'];
+companies.pop(1);
+console.log(companies);
+companies.shift();
+companies.splice(1,2,'netflix','spotify');
+console.log(companies);
+companies.push("tesla");
+console.log(companies);
+console.log(companies.indexOf('amazon'));
 
-// Day 8 // functions in JS
+Day 8 // functions in JS
 
-// function greeet(){
-//      console.log("Hello, welcome to JavaScript programming");
-//      console.log("Have a great day!");
-// }
-// greeet();
-// greeet();
+function greeet(){
+     console.log("Hello, welcome to JavaScript programming");
+     console.log("Have a great day!");
+}
+greeet();
+greeet();
 
-// function myMsg(msg){
-//      console.log(msg);
-// }
-// myMsg("This is my first function with parameter");
+function myMsg(msg){
+     console.log(msg);
+}
+myMsg("This is my first function with parameter");
 
-// function sum(a,b){
-//     console.log("before return");
-//      return a+b;
-//      console.log("after return"); // unreachable code
-// }
-// let result=sum(5,10);
-// console.log("Sum is:",result);
+function sum(a,b){
+    console.log("before return");
+     return a+b;
+     console.log("after return"); // unreachable code
+}
+let result=sum(5,10);
+console.log("Sum is:",result);
 
-// // arrow function 
+// arrow function 
 
-// (a,b)=>{
-//      return a+b;
-// }
-// // or
-// let add=(a,b)=> a+b;
-// let res=add(7,3);
-// console.log("Addition is:",res);
+(a,b)=>{
+     return a+b;
+}
+// or
+let add=(a,b)=> a+b;
+let res=add(7,3);
+console.log("Addition is:",res);
 
-// let mul=(a,b)=> {
-//      console.log(a*b);
-// return a*b;};
-// console.log("Multiplication is:",mul(5,4));
+let mul=(a,b)=> {
+     console.log(a*b);
+return a*b;};
+console.log("Multiplication is:",mul(5,4));
 
-// // practice problem:
-// function countVowels(str){
-//      let count = 0;
-//      let vowels = 'aeiouAEIOU';
-//      for (let i = 0; i < str.length; i++){
-//           if(vowels.includes(str[i])){
-//                count++;
-//           }
-//      }
-//      return count;  // ✅ return the count
-// }
+// practice problem:
+function countVowels(str){
+     let count = 0;
+     let vowels = 'aeiouAEIOU';
+     for (let i = 0; i < str.length; i++){
+          if(vowels.includes(str[i])){
+               count++;
+          }
+     }
+     return count;  // ✅ return the count
+}
 
-// let string = "Hello World";
-// let count = countVowels(string);
-// console.log("Number of vowels in the string is:", count);
+let string = "Hello World";
+let count = countVowels(string);
+console.log("Number of vowels in the string is:", count);
 
-// let vCount=(str)=>{
-//      count=0;
-//    let vowels = 'aeiouAEIOU';
-//    for (i=0;i<=str.length;i++){
-//      if(vowels.includes(str[i])){
-//           count++;
-//      }
-//    }
-//    return count;
-// }
-// let stringh='Rajib Shrestha'
-// let countt=vCount(stringh)
-// console.log(count)
+let vCount=(str)=>{
+     count=0;
+   let vowels = 'aeiouAEIOU';
+   for (i=0;i<=str.length;i++){
+     if(vowels.includes(str[i])){
+          count++;
+     }
+   }
+   return count;
+}
+let stringh='Rajib Shrestha'
+let countt=vCount(stringh)
+console.log(count)
 
-// day 9 // deep dive into js
+day 9 // deep dive into js
 
-// let arr=[1,2,3,4,5];
-// arr.forEach(function printVal(val){
-//      console.log(val);   
-// });
+let arr=[1,2,3,4,5];
+arr.forEach(function printVal(val){
+     console.log(val);   
+});
 
-// let arrr=["kathmandu","pokhara","dhading","biratnagar"];
-// arrr.forEach(function pCity(city){
-//      console.log(city.toUpperCase());
+let arrr=["kathmandu","pokhara","dhading","biratnagar"];
+arrr.forEach(function pCity(city){
+     console.log(city.toUpperCase());
+})
+// map function
+let squaredArr=arr.map(function squareVal(val){
+     return val*val;
+});
+console.log(squaredArr);
+// filter function
+let evenArr=arr.filter(function filterEven(val){
+     return val%2===0;
+});
+console.log(evenArr);
+// reduce function
+let sumArr=arr.reduce(function sumVal(accumulator,val){
+     return accumulator+val;
+},0);
+// console.log(sumArr);
+
+// const output=arr.reduce((prev,curr)=>{
+//      return prev> curr? prev : curr;
 // })
-// // map function
-// let squaredArr=arr.map(function squareVal(val){
-//      return val*val;
-// });
-// console.log(squaredArr);
-// // filter function
-// let evenArr=arr.filter(function filterEven(val){
-//      return val%2===0;
-// });
-// console.log(evenArr);
-// // reduce function
-// let sumArr=arr.reduce(function sumVal(accumulator,val){
+// console.log(output);
+
+// // practice problem:
+// let marks=[85,97,44,37,96,60];
+// let a=marks.filter((val)=> {
+//      return val>=90});
+// console.log(a);
+
+// let n= prompt("enter your number: ");
+// let aer=[];
+// for (let i=1;i<=n;i++){
+//      aer[i-1]=i;    
+// }
+// console.log(aer);
+// let sumaer=aer.reduce(function sumVall(accumulator,val){
 //      return accumulator+val;
-// },0);
-// // console.log(sumArr);
+// })
+// console.log("sum:",sumaer);
+// let facaer=aer.reduce(function factArr(acc,val){
+//      return acc*val;
+// })
+// console.log("factorial:",facaer);\
 
-// // const output=arr.reduce((prev,curr)=>{
-// //      return prev> curr? prev : curr;
-// // })
-// // console.log(output);
+// Day 10 --DOM-document object model
 
-// // // practice problem:
-// // let marks=[85,97,44,37,96,60];
-// // let a=marks.filter((val)=> {
-// //      return val>=90});
-// // console.log(a);
+// console.log(window.document)
 
-// // let n= prompt("enter your number: ");
-// // let aer=[];
-// // for (let i=1;i<=n;i++){
-// //      aer[i-1]=i;    
-// // }
-// // console.log(aer);
-// // let sumaer=aer.reduce(function sumVall(accumulator,val){
-// //      return accumulator+val;
-// // })
-// // console.log("sum:",sumaer);
-// // let facaer=aer.reduce(function factArr(acc,val){
-// //      return acc*val;
-// // })
-// // console.log("factorial:",facaer);\
+// // Day 11 --Dom - practical
 
-// // Day 10 --DOM-document object model
+// console.dir(document.body);
+// console.log(document.body);
+// console.log(document.head);
+// console.dir(document.body.childNodes[1]);
 
-// // console.log(window.document)
+// document.body.childNodes[1].innerText="DIV 1";
 
-// // // Day 11 --Dom - practical
+// let heading = document.getElementById("header");//h1
 
-// // console.dir(document.body);
-// // console.log(document.body);
-// // console.log(document.head);
-// // console.dir(document.body.childNodes[1]);
+// console.dir(heading);
 
-// // document.body.childNodes[1].innerText="DIV 1";
+// let button = document.getElementById("my id");//h1
+// console.dir(button);
+// let header = document.getElementsByClassName("heading-class");
+// console.dir(header);
+// console.log(header);
+// let allEl=document.querySelectorAll(".myClass");
+// console.dir(allEl);
 
-// // let heading = document.getElementById("header");//h1
+// console.dir(document.body.firstChild)
 
-// // console.dir(heading);
+// // practice qn
 
-// // let button = document.getElementById("my id");//h1
-// // console.dir(button);
-// // let header = document.getElementsByClassName("heading-class");
-// // console.dir(header);
-// // console.log(header);
-// // let allEl=document.querySelectorAll(".myClass");
-// // console.dir(allEl);
+// let h2=document.querySelector("h2");
+// console.log(h2);
+// console.dir(h2.innerText);
+// h2.innerText=h2.innerText + "From Rajib Shrestha";
 
-// // console.dir(document.body.firstChild)
+// let divs=document.querySelectorAll(".box");
+// console.dir(divs);
+// console.dir(divs[2])
+// let idx=0;
+// for (div of divs){
+//      console.dir(div.innerText);
+//      div.innerText=`new unique valur ${idx}`;
+//      idx++;
+// }
+// // divs[0].innerText="new unique value 1";
+// // divs[1].innerText="new unique value 2";
+// // divs[2].innerText="new unique value 3";
+// console.dir(divs);
 
-// // // practice qn
+// Day 11
+let div=document.querySelector("div");
+console.log(div);
+console.log(div.style)
+div.style.backgroundColor="yellow";
+div.style.fontSize="30px";
+div.style.visibility="";
 
-// // let h2=document.querySelector("h2");
-// // console.log(h2);
-// // console.dir(h2.innerText);
-// // h2.innerText=h2.innerText + "From Rajib Shrestha";
+let id=div.getAttribute("id");
+console.log(id);
+let name=div.getAttribute("name");
+console.log(name);
 
-// // let divs=document.querySelectorAll(".box");
-// // console.dir(divs);
-// // console.dir(divs[2])
-// // let idx=0;
-// // for (div of divs){
-// //      console.dir(div.innerText);
-// //      div.innerText=`new unique valur ${idx}`;
-// //      idx++;
-// // }
-// // // divs[0].innerText="new unique value 1";
-// // // divs[1].innerText="new unique value 2";
-// // // divs[2].innerText="new unique value 3";
-// // console.dir(divs);
+let paragraph=document.querySelector("p");
+console.log(paragraph.getAttribute("class"));
+paragraph.setAttribute("class","123");
 
-// // Day 11
-// let div=document.querySelector("div");
-// console.log(div);
-// console.log(div.style)
-// div.style.backgroundColor="yellow";
-// div.style.fontSize="30px";
-// div.style.visibility="";
+let  newBtn=document.createElement("button");
+newBtn.innerText="clickme!";
+console.log(newBtn)
+console.log(newBtn.innerText);
+div.after(newBtn);
 
-// let id=div.getAttribute("id");
-// console.log(id);
-// let name=div.getAttribute("name");
-// console.log(name);
+let newHeading = document.createElement("h2");
+newHeading.innerText="helloworld";
+document.querySelector("body").prepend(newHeading)
 
-// let paragraph=document.querySelector("p");
-// console.log(paragraph.getAttribute("class"));
-// paragraph.setAttribute("class","123");
+paragraph.remove()
 
-// let  newBtn=document.createElement("button");
-// newBtn.innerText="clickme!";
-// console.log(newBtn)
-// console.log(newBtn.innerText);
-// div.after(newBtn);
+// practice qn:
 
-// let newHeading = document.createElement("h2");
-// newHeading.innerText="helloworld";
-// document.querySelector("body").prepend(newHeading)
+let newButton=document.createElement("button");
+newButton.innerText="Click me";
+newButton.style.backgroundColor="red";
+newButton.style.color="white";
 
-// paragraph.remove()
+document.querySelector("body").prepend(newButton);
 
-// // practice qn:
+let bewPara=document.createElement("paragraph");
+bewPara.innerText="Hello Rajib";
+bewPara.backgroundColor="red";
 
-// let newButton=document.createElement("button");
-// newButton.innerText="Click me";
-// newButton.style.backgroundColor="red";
-// newButton.style.color="white";
+// qn 2
 
-// document.querySelector("body").prepend(newButton);
+let para=document.querySelector("p")
 
-// let bewPara=document.createElement("paragraph");
-// bewPara.innerText="Hello Rajib";
-// bewPara.backgroundColor="red";
+Day 13 Events in javascript
 
-// // qn 2
-
-// let para=document.querySelector("p")
