7a06ad8aed2e7fec89ec51d8817400beb7a20b11
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-01-09 20:46:38 +0545

    day 13

--- JAVA_SCRIPT/jscode2.js
+++ JAVA_SCRIPT/jscode2.js
@@ -1,15 +1,45 @@
 // Day 13 event
 
-console.log("hello world");
-let btn1 = document.querySelector("#btn1");
+// console.log("hello world");
+// let btn1 = document.querySelector("#btn1");
 
-if (btn1) {
-    let clickCount = 0;  // Declare outside to persist
-    btn1.onclick = () => {
-        console.log("btn 1 was clicked");
-        clickCount++;
-        console.log("Click count:", clickCount);
-    };
-} else {
-    console.log("Button with id='btn1' not found!");
+// if (btn1) {
+//     let clickCount = 0;  // Declare outside to persist
+//     btn1.onclick = () => {
+//         console.log("btn 1 was clicked");
+//         clickCount++;
+//         console.log("Click count:", clickCount);
+//     };
+// } else {
+//     console.log("Button with id='btn1' not found!");
+// }
+
+// Day 14 Deep dive into Event
+
+let btn1=document.querySelector("#btn1");
+
+btn1.addEventListener("click",(evt) =>{
+    console.log("button 1 was clicked ")
+    console.log(evt);
+    console.log(evt.type);
+})
+btn1.addEventListener("click",() =>{
+    console.log("button 1 was clicked handler 2")
+})
+
+btn1.addEventListener("click",() =>{
+    console.log("button 1 was clicked handler 4")
+})
+
+// btn1.removeEventListener("click",()=>{
+    //     console.log("button was removed 3")  "wrong"
+    // })
+    
+const handler3=() =>{
+    console.log("button 1 was clicked handler 3")
 }
+btn1.addEventListener("click",handler3);
+btn1.removeEventListener("click",handler3);
+
+// practice question
+
