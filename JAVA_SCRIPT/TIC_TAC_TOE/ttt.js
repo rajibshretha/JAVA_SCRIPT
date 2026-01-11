db074713584af74bf494b98c25f45181048aad3c
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-01-11 22:15:36 +0545

    day 16

--- JAVA_SCRIPT/TIC_TAC_TOE/ttt.js
+++ JAVA_SCRIPT/TIC_TAC_TOE/ttt.js
@@ -1,6 +1,8 @@
 let boxes=document.querySelectorAll(".box");
-let resetBtn=document.querySelector("#reset-btn");
-
+let resetBtn=document.querySelector("#reset");  // Changed from #reset-btn
+let newGameBtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
 let turnO=true;
 
 const winPatters=[
@@ -18,18 +20,52 @@ boxes.forEach((box) => {
      box.addEventListener("click", () => {
           console.log("box was clicked");
           if(turnO){
-               box.innerText="O";//player0
+               box.innerText="O";
                turnO=false;
           } else {
                box.innerText="X";
                turnO=true;
           }
           box.disabled = true;
+          checkWinner();  // Added this line
      });
 });
 
-const checkWinner=()=>{
-     for(let pattern of winPatters){
-          console.log(boxespattern[0],boxespattern[1],boxespattern[2]);
-     }
+const showWinner=(winner)=>{
+     msg.innerText=`Congratulations, Winner is ${winner}`;
+     msgContainer.classList.remove("hide");
 }
+
+const checkWinner = () => {
+     for (let pattern of winPatters){
+          let pos1Val=boxes[pattern[0]].innerText;  // Fixed
+          let pos2Val=boxes[pattern[1]].innerText;  // Fixed
+          let pos3Val=boxes[pattern[2]].innerText;  // Fixed
+          
+          if (pos1Val !="" && pos2Val !="" && pos3Val !=""){
+               if (pos1Val===pos2Val && pos2Val===pos3Val){
+                    console.log("winner",pos1Val)
+                    showWinner(pos1Val);  // Pass the winner
+               }
+          }
+     }
+};
+
+// Add reset functionality
+resetBtn.addEventListener("click", () => {
+     turnO = true;
+     boxes.forEach((box) => {
+          box.innerText = "";
+          box.disabled = false;
+     });
+     msgContainer.classList.add("hide");
+});
+
+newGameBtn.addEventListener("click", () => {
+     turnO = true;
+     boxes.forEach((box) => {
+          box.innerText = "";
+          box.disabled = false;
+     });
+     msgContainer.classList.add("hide");
+});
