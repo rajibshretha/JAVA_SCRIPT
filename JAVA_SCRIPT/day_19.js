32c02048a1a3e1d55ce810324fe50823fe6d0075
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-01-21 17:13:38 +0545

    day 24

--- JAVA_SCRIPT/day_19.js
+++ JAVA_SCRIPT/day_19.js
@@ -165,35 +165,98 @@
 
 // Day 23 
 
-let promise = new Promise((resolve,reject)=>{
-     console.log("Iam a promise.")
-     reject("some error occurred")
-})
+// let promise = new Promise((resolve,reject)=>{
+//      console.log("Iam a promise.")
+//      reject("some error occurred")
+// })
 
-function getData(dataId,getNextData){
+// function getData(dataId,getNextData){
+//      return new Promise((resolve,reject)=>{
+//           setTimeout(()=>{
+//                console.log("data",dataId);
+//                resolve("success");
+//           if (getNextData){
+//                getNextData();
+//           }
+//           },2000);
+//      });
+// }
+
+// const getPromise=()=>{
+//      return new Promise((resolve,reject)=>{
+//           console.log("Iam a promise.");
+//           // resolve("Success");
+//           reject("error")
+//      });
+// };
+
+// let promisee = getPromise();
+// promise.then(()=>{
+//      console.log("Promise fulfilled")
+// })
+// promise.catch(()=>{
+//      console.log("rejected")
+// })
+
+// Day 24
+
+function asyncFunc1(){
      return new Promise((resolve,reject)=>{
           setTimeout(()=>{
-               console.log("data",dataId);
+               console.log("some data 1");
                resolve("success");
-          if (getNextData){
-               getNextData();
-          }
-          },2000);
+          },4000);
      });
 }
-
-const getPromise=()=>{
+function asyncFunc2(){
      return new Promise((resolve,reject)=>{
-          console.log("Iam a promise.");
-          // resolve("Success");
-          reject("error")
+          setTimeout(()=>{
+               console.log("some data 2");
+               resolve("success");
+          },4000);
      });
-};
+}
+// console.log("fetching data 1")
+// let p1=asyncFunc1();
+// p1.then((result)=>{
+//      console.log(result)
+// })
+// console.log("fetching data 2") //wrong
+// let p2=asyncFunc2();
+// p1.then((result)=>{
+//      console.log(result)
+// })
 
-let promisee = getPromise();
-promise.then(()=>{
-     console.log("Promise fulfilled")
+console.log("fetching data 1")
+let p1=asyncFunc1();
+p1.then((result)=>{
+     console.log(result)
+     console.log("fetching data 2");
+     let p2=asyncFunc2();
+     p2.then((result)=>{
+          console.log("result")
+     })
 })
-promise.catch(()=>{
-     console.log("rejected")
+
+// or 
+// promise chain
+console.log("fetching data 1 ...");
+asyncFunc1().then((result)=>{
+     console.log("fetching data 2 ...");
+     asyncFunc2().then((res)=>{});
 })
+
+function api(){
+     return new Promise((resolve,reject)=>{
+          setTimeout(()=>{
+               console.log("weather data");
+               resolve(200);
+          },2000);
+     })
+}
+async function getWeatherData(){
+     await api()
+     await api()
+}
+
+getWeatherData()
