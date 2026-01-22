4a4a1037446a09bcddeeb3debc5b8638a7e4cdf5
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-01-22 16:34:47 +0545

    day 25

--- JAVA_SCRIPT/day_19.js
+++ JAVA_SCRIPT/day_19.js
@@ -201,22 +201,22 @@
 
 // Day 24
 
-function asyncFunc1(){
-     return new Promise((resolve,reject)=>{
-          setTimeout(()=>{
-               console.log("some data 1");
-               resolve("success");
-          },4000);
-     });
-}
-function asyncFunc2(){
-     return new Promise((resolve,reject)=>{
-          setTimeout(()=>{
-               console.log("some data 2");
-               resolve("success");
-          },4000);
-     });
-}
+// function asyncFunc1(){
+//      return new Promise((resolve,reject)=>{
+//           setTimeout(()=>{
+//                console.log("some data 1");
+//                resolve("success");
+//           },4000);
+//      });
+// }
+// function asyncFunc2(){
+//      return new Promise((resolve,reject)=>{
+//           setTimeout(()=>{
+//                console.log("some data 2");
+//                resolve("success");
+//           },4000);
+//      });
+// }
 // console.log("fetching data 1")
 // let p1=asyncFunc1();
 // p1.then((result)=>{
@@ -228,35 +228,35 @@ function asyncFunc2(){
 //      console.log(result)
 // })
 
-console.log("fetching data 1")
-let p1=asyncFunc1();
-p1.then((result)=>{
-     console.log(result)
-     console.log("fetching data 2");
-     let p2=asyncFunc2();
-     p2.then((result)=>{
-          console.log("result")
-     })
-})
+// console.log("fetching data 1")
+// let p1=asyncFunc1();
+// p1.then((result)=>{
+//      console.log(result)
+//      console.log("fetching data 2");
+//      let p2=asyncFunc2();
+//      p2.then((result)=>{
+//           console.log("result")
+//      })
+// })
 
-// or 
-// promise chain
-console.log("fetching data 1 ...");
-asyncFunc1().then((result)=>{
-     console.log("fetching data 2 ...");
-     asyncFunc2().then((res)=>{});
-})
+// // or 
+// // promise chain
+// console.log("fetching data 1 ...");
+// asyncFunc1().then((result)=>{
+//      console.log("fetching data 2 ...");
+//      asyncFunc2().then((res)=>{});
+// })
 
-function api(){
-     return new Promise((resolve,reject)=>{
-          setTimeout(()=>{
-               console.log("weather data");
-               resolve(200);
-          },2000);
-     })
-}
-async function getWeatherData(){
-     await api()
-     await api()
-}
-getWeatherData()
+// function api(){
+//      return new Promise((resolve,reject)=>{
+//           setTimeout(()=>{
+//                console.log("weather data");
+//                resolve(200);
+//           },2000);
+//      })
+// }
+// async function getWeatherData(){
+//      await api()
+//      await api()
+// }
+// getWeatherData()
