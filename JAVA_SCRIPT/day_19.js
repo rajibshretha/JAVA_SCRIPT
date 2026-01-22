0624436cd1c9a6941b63995c3bc0ebfbf417a0bd
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-01-22 20:45:57 +0545

    day 25

--- JAVA_SCRIPT/day_19.js
+++ JAVA_SCRIPT/day_19.js
@@ -260,3 +260,20 @@
 //      await api()
 // }
 // getWeatherData()
+
+
+// Day 25
+
+const URL = "https://meowfacts.herokuapp.com";
+const factPara=document.querySelector("#fact");
+const btn=document.querySelector("#btn");
+
+
+const getFacts = async () => {
+    console.log("getting data.....")
+    let response = await fetch(URL);
+    console.log(response);//JSON format
+    let data = await response.json();
+    factPara.innerText=data[0].text;
+};
+btn.addEventListener("click",getFacts);
