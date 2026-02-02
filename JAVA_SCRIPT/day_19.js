6d0a8168a262db66c03dbd5e454b1a1abfd998d6
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-02-02 21:38:34 +0545

    day 30

--- JAVA_SCRIPT/day_19.js
+++ JAVA_SCRIPT/day_19.js
@@ -293,6 +293,8 @@ console.dir(header1)
 
 const para1=document.getElementsByClassName("paragraph1");
 console.dir(para1)
+console.dir(para1)
+console.dir(para1)
 
 
 // cd "c:\CODING\WEB DEVELOPMENT\SELF LEARN HTML-CSS-JAVASCRIPT\JAVA SCRIPT\java_script\JAVA_SCRIPT"
