216981209f1456d362695f801f09abad4641c882
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-02-23 17:45:47 +0545

    DAY 30

--- JAVA_SCRIPT/day_19.js
+++ JAVA_SCRIPT/day_19.js
@@ -430,6 +430,14 @@ console.dir(para1);
 console.dir(para1);
 console.dir(para1);
 console.dir(para1);
+console.dir(para1);
+console.dir(para1);
+console.dir(para1);
+console.dir(para1);
+console.dir(para1);
+console.dir(para1);
+console.dir(para1);
+console.dir(para1);
 console.dir(para1)
 console.dir(para1);
 console.dir(para1);
