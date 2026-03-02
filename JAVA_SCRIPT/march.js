f1d99e70f7cd31f61b1778f2aed7d1b09bc9d929
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-03-02 21:28:15 +0545

    day 31

---
+++ JAVA_SCRIPT/march.js
@@ -0,0 +1,5 @@
+function add(){
+     var a = document.getElementById("inp1").value;
+     var b = document.getElementById("inp1").value;
+     var c = parseInt(a)+parseInt(b);
+     document.getElementById("result").innerHTML= c ;}
